40e28e28e1c3e330f003afd9edf1322823da9938
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-05-01 09:40:17 +0000

    fix: remove wrangler.toml to resolve cloudflare pages conflict (huftt)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,7 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    mode: 'directory',
-  }),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
